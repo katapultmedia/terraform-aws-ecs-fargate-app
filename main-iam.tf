11da3c82edd566727dc0170c78ffceb6ff65bb18
Warren Brown <warren@katapultmedia.com>
2020-07-14 14:31:37 -0500

    chore: added test folder

--- main-iam.tf
+++ main-iam.tf
@@ -12,8 +12,8 @@ data "aws_iam_policy_document" "ssm_exec" {
 }
 
 resource "aws_iam_role_policy" "hasura_exec_ssm" {
-  name   = "${module.alb_service_task_hasura.ecs_exec_role_policy_name}-ssm-policy"
+  name   = "${module.alb_service_task.ecs_exec_role_policy_name}-ssm-policy"
   policy = data.aws_iam_policy_document.ssm_exec.json
-  role   = module.alb_service_task_hasura.ecs_exec_role_policy_name
+  role   = module.alb_service_task.ecs_exec_role_policy_name
 }
 
